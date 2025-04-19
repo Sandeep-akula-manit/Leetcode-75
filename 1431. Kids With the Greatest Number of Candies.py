fb219f32b3856bceaa647522d1b5eb8fcb8681ad
Sandeep-akula-manit <sandeep.akula.manit@gmail.com>
2025-04-18 22:48:10 -0600

    Update 1431. Kids With the Greatest Number of Candies.py

--- 1431. Kids With the Greatest Number of Candies.py
+++ 1431. Kids With the Greatest Number of Candies.py
@@ -1,12 +1,9 @@
 def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
     # Find the maximum number of candies in the list
     max_candies = max(candies)
-
     # Create a result list
     result = []
-
     # Compare each kid's candies with the maximum number of candies after adding the extra candies
     for i in range(len(candies)):
         result.append(candies[i] + extraCandies >= max_candies)
-    
     return result
